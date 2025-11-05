08b56209354d3a1882071730c6916459b847b2db
Tyler Cain <tylercain67@gmail.com>
2025-11-04 21:57:11 -0600

    changes

--- lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
-
-const supabase = createClient(process.env.PUBLIC_KEY as any, process.env.SERVICE_ROLE as any)
-
-export const getSupabaseClient = ()=>{
-    return supabase;
-}
