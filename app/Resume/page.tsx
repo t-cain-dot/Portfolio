42888af3bcf0607327e4c0d5ef2d3c65613d61bf
Tyler Cain <tylercain67@gmail.com>
2025-11-06 12:33:47 -0600

    closingquote

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,5 +1,5 @@
 export default function ResumePage() {
-  const pdfUrl = "https://xywzbpmzpbcqlvikdbkh.supabase.co/storage/v1/object/public/Resume/Tyler_Cain_Resume%20(2).pdf
+  const pdfUrl = "https://xywzbpmzpbcqlvikdbkh.supabase.co/storage/v1/object/public/Resume/Tyler_Cain_Resume%20(2).pdf"
 
   return (
     <div style={{ width: "100%", height: "100vh", padding: "1rem" }}>
