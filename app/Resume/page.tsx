84681ed76c72c74ab0bd1161fbb0fa71d9028671
Tyler Cain <tylercain67@gmail.com>
2025-11-06 12:32:02 -0600

    pdf

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,10 +1,10 @@
 export default function ResumePage() {
-  const pdfUrl = "https://xywzbpmzpbcqlvikdbkh.supabase.co/storage/v1/object/sign/Resume/Tyler_Cain_Resume%20(2).pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV80YmFiNDNmMC03MTliLTQ1NmQtYTBjYS1iZDIxYWJkZWFmNWIiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJSZXN1bWUvVHlsZXJfQ2Fpbl9SZXN1bWUgKDIpLnBkZiIsImlhdCI6MTc2MjQ1MjM0MiwiZXhwIjoxNzkzOTg4MzQyfQ.QIhYietgfcVdt9cPX54ceynL8MLdKrBwDvI37iFS5H4"
+  const pdfUrl = "https://xywzbpmzpbcqlvikdbkh.supabase.co/storage/v1/object/public/Resume/Tyler_Cain_Resume%20(2).pdf
 
   return (
     <div style={{ width: "100%", height: "100vh", padding: "1rem" }}>
       <iframe
-        src={pdfUrl}
+        src={pfURL}
         width="100%"
         height="100%"
         style={{ border: "none" }}
