ea025254ea6059fc16825b723dbb1314b8575dc4
Tyler Cain <tylercain67@gmail.com>
2025-11-06 13:21:16 -0600

    poop

--- app/Tyler/page.tsx
+++ app/Tyler/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 export default function ResumePage() {
-//   const pdfUrl = "https://xywzbpmzpbcqlvikdbkh.supabase.co/storage/v1/object/public/Resume/TylerCainResume.pdf"
+   const pdfUrl = "https://xywzbpmzpbcqlvikdbkh.supabase.co/storage/v1/object/public/Resume/TylerCainResume.pdf"
 
   return (
     <div style={{ width: "100%", height: "100vh", padding: "1rem" }}>
-      {/* <iframe
+      <iframe
         src={pdfUrl}
         width="100%"
         height="100%"
         style={{ border: "none" }}
         title="Resume PDF"
-      /> */}
+      />
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react"
 const projects = [
   {
     name: "Resume",
-    href: "/tyler",
+    href: "/Tyler",
     description: "Explore creative experiences",
     icon: "✨",
   },
